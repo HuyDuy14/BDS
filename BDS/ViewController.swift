b5fffe1c6cac9ab13673cf3393a48eb7be00ce21
Duy Huy <huyld@ominext.com>
2017-11-29 22:00:21 +0700

    Initial Commit

---
+++ BDS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BDS
+//
+//  Created by Duy Huy on 11/29/17.
+//  Copyright © 2017 Duy Huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
