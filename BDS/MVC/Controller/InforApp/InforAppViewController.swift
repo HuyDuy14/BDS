169a4f11776c7d620d2283939d68dd4147e3760d
Duy Huy <huyld@ominext.com>
2017-12-09 21:27:42 +0700

    progress webview

--- BDS/MVC/Controller/InforApp/InforAppViewController.swift
+++ BDS/MVC/Controller/InforApp/InforAppViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class InforAppViewController: BaseViewController {
+class InforAppViewController: BaseViewController,UIWebViewDelegate {
 
+    @IBOutlet weak var progress: UIActivityIndicatorView!
     @IBOutlet weak var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         if let url = URL(string: "https://chobatdongsan.com.vn/gioi-thieu-st36") {
             let request = URLRequest(url: url)
             webView.loadRequest(request)
+            webView.translatesAutoresizingMaskIntoConstraints = false
+            webView.isUserInteractionEnabled = true
+            webView.delegate = self
+            self.webView.addSubview(self.progress)
+            self.progress.startAnimating()
+            self.progress.hidesWhenStopped = true
         }
     }
 
     @IBAction func backButtonDidTap(_ sender: Any) {
         self.popToView()
     }
+    
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+         progress.stopAnimating()
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+         progress.stopAnimating()
+    }
+ 
 }
